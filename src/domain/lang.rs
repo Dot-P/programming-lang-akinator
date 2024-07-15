6c6ce72d657f66fa4e6cc2545fa33b9b00b4a8e4
Dot-P <metaalice01@gmail.com>
2024-07-15 11:26:24 +0900

    Feat: add question struct and remodel the relevant process

--- src/domain/lang.rs
+++ src/domain/lang.rs
@@ -4,11 +4,18 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "camelCase")]
 pub struct Language{
     pub name: String,
-    pub question: Vec<String>
+    pub answer: Vec<String>
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QuestionList {
+    pub question_list: Vec<String>
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct DB {
-    pub language_list: Vec<Language>
+    pub language_list: Vec<Language>,
+    pub question_list: Vec<String>
 }
--- src/main.rs
+++ src/main.rs
@@ -20,27 +20,34 @@ fn main() {
     // jsonファイルからプログラミング言語情報を取り出す
     const FILEPATH: &str= "./src/language.json";
 
-    // 問題番号を決める
-    let qnum = 1;
-
-    // 問題を出力
-    match load_json(FILEPATH) {
-        Ok(db) => {
-            println!("name: {}", db.language_list[qnum].name);
-            for q in db.language_list[qnum].question.iter() {
-                println!("question: {}", q);
-            }
-        },
+    let db = match load_json(FILEPATH) {
+        Ok(db) => db,
         Err(err) => {
-            println!("Error: {}", err);
+            eprintln!("Error: {}", err);
+            return;
         }
+    };
+
+    let total_question: usize = db.question_list.len();
+
+    // 答えが決まるまで質問を繰り返す
+    let mut qnum = 0;
+    for i in 1..total_question {
+        println!("================================================================================");
+        println!("{}", db.question_list[qnum]);
+
+        // 標準入力を受け取る
+        println!("1: はい 2: 多分そう部分的にそう 3: 分からない 4: 多分違うそうでもない 5: いいえ");
+        print!(">> ");
+        stdout().flush().unwrap(); 
+        let ans = input();
+
+        // 答えをもとに言語の候補を絞る(todo)
+
+        // 回答から次の問題番号を決める(todo)
+        qnum = i;
     }
 
-    // 標準入力を受け取る
-    print!(">> ");
-    stdout().flush().unwrap(); 
-    let ans = input();
-
     // 最終結果を出力
-    println!("Your answer: {}", ans);
+    println!("Some output");
 }
